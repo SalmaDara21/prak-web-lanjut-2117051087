2924fca206dc4c431a706629ff768684153cddc8
SalmaDara21 <salma.dara21@students.unila.ac.id>
2023-09-18 23:26:06 +0800

    form-controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,10 +1,17 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\UserController;
+use App\Controllers\Home;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 $routes->get('/profile', 'Home::profile');
-#$routes->get('profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
+$routes->get('/user/profile', [UserController::class, 'profile']);
+$routes->get('/user/create', [UserController::class, 'create']);
+
+$routes->post('/user/store', 'UserController::store');
+
+
